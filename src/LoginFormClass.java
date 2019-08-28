fd2687a91e4aeebe45be3ad2ed395bbd48420a3c
Ravikumar <30296080+RaviMark@users.noreply.github.com>
2019-08-28 19:49:05 +0530

    Add files via upload

---
+++ src/LoginFormClass.java
@@ -0,0 +1,54 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class LoginFormClass
+ */
+@WebServlet("/LoginFormClass")
+public class LoginFormClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public LoginFormClass() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		String username=request.getParameter("Username");
+		String password=request.getParameter("Password");
+		
+		ServletContext servletContext = getServletContext();
+		servletContext.setAttribute("User", username);
+        
+        RequestDispatcher rd = request.getRequestDispatcher("/myFile.jsp");
+        rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
